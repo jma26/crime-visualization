931eb25f3abd54fef529ab52269ced37a94eaf0a
Jesse Ma <j.masonoma17@gmail.com>
2019-01-23 22:50:35 -0800

    Refactor methods and dynamically update crimeData state dynamically at Dashboard component

--- crime-visualization/src/components/Dashboard.vue
+++ crime-visualization/src/components/Dashboard.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-
+import axios from 'axios';
 import Header from './Header.vue';
 import Footer from './Footer.vue';
 
@@ -20,53 +20,46 @@ export default {
   data() {
     return {
       crimeData: {
-        city: {
-          burglary_count: null,
-          arson_count: null,
-          motor_vehicle_theft_count: null,
-          larceny_count: null,
-          homicide_count: null,
-          robbery_count: null,
-          aggravated_assault_count: null,
-          rape_count: null
-        },
-        state: {
-          burglary_count: null,
-          arson_count: null,
-          motor_vehicle_theft_count: null,
-          larceny_count: null,
-          homicide_count: null,
-          robbery_count: null,
-          aggravated_assault_count: null,
-          rape_count: null,
-        }
+        city: {},
+        state: {}
       }
     }
   },
   methods: {
-    Test() {
-      axios.get('/').then((data) => {
-        console.log(data);
+    getCityData() {
+      axios.get('/citydata').then(response => {
+        let data = response.data.data;
+        if (response.status === 200 && response.data.success === true) {
+          // Loop over length
+          for (let i = 0; i < data.length; i++) {
+            // Loop over properties
+            for (let prop in data[i]) {
+              // Set reactive properties to object
+              this.$set(this.crimeData.city, prop, data[i][prop]);
+            }
+          }
+        }
       })
-      console.log('Hi');
     },
-    getCityData: async function() {
-
-      const CityApiPromises = Promise.all([await CityAPI.burglary(), await CityAPI.arson(), await CityAPI.motor_vehicle_theft(), await CityAPI.larceny(), await CityAPI.homicide(), await CityAPI.robbery(), await CityAPI.aggravated_assault(), await CityAPI.rape()])
-
-      this.crimeData.city.burglary_count, this.crimeData.city.arson_count, this.crimeData.city.motor_vehicle_theft_count, this.crimeData.city.larceny_count, this.crimeData.city.homicide_count, this.crimeData.city.robbery_count, this.crimeData.city.aggravated_assault_count, this.crimeData.city.rape_count = await CityApiPromises;
-    },
-    getStateData: async function() {
-
-      const StateApiPromises = Promise.all([await StateAPI.burglary(), await StateAPI.arson(), await StateAPI.motor_vehicle_theft(), await StateAPI.larceny(), await StateAPI.homicide(), await StateAPI.robbery(), await StateAPI.aggravated_assault(), await StateAPI.rape()])
-
-      this.crimeData.state.burglary_count, this.crimeData.state.arson_count, this.crimeData.state.motor_vehicle_theft_count, this.crimeData.state.larceny_count, this.crimeData.state.homicide_count, this.crimeData.state.robbery_count, this.crimeData.state.aggravated_assault_count, this.crimeData.state.rape_count = await StateApiPromises;
+    getStateData() {
+      axios.get('/statedata').then(response => {
+        let data = response.data.data;
+        if (response.status === 200 && response.data.success === true) {
+          // Loop over length
+          for (let i = 0; i < data.length; i++) {
+            // Loop over properties
+            for (let prop in data[i]) {
+              // Set reactive properties to object
+              this.$set(this.crimeData.state, prop, data[i][prop]);
+            }
+          }
+        }
+      })
     }
   },
   created() {
     this.getCityData();
     this.getStateData();
-    this.Test();
   }
 }
 </script>
