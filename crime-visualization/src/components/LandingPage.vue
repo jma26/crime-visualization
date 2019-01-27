1d33f6aad2f6c3bdcff9bf85302c9d9bb1cc9f65
Jesse Ma <j.masonoma17@gmail.com>
2019-01-27 01:42:48 -0800

    Update landingpage headline and content

--- crime-visualization/src/components/LandingPage.vue
+++ crime-visualization/src/components/LandingPage.vue
@@ -1,22 +1,14 @@
 <template>
   <div id="landingpage">
     <div class="landingpage__logo-container">
-      <img src="../assets/seacrime-logo.png" alt="SeaCrime - Crime Visualization of Seattle">
+      <img class="landingpage__image--logo" src="../assets/seacrime-logo.png" alt="SeaCrime - Crime Visualization of Seattle">
     </div>
     <section class="landingpage__section">
       <article class="landingpage__article">
-        <p class="landingpage__content">
-          Introducing SeaCrime, 
-          <br>
-          Seattle's Crime Visualization
-          <!-- a new crime data visualization to how safe Seattle is -->
-        </p>
-        <!-- <p class="landingpage__content">Data provided by <a href="https://crime-data-explorer.fr.cloud.gov/" target="_blank">FBI's Uniform Crime Reporting (UCR) Program</a></p> -->
+        <h1 class="landingpage__headline">Introducing SeaCrime.</h1>
+        <p class="landingpage__content">Visualizing Seattle's crime data in graphs</p>
         <input class="landingpage__button--green landingpage__button--desktop" type="button" value="Check it out!" v-on:click="toDashboardPage">
       </article>
-      <div class="landingpage__demo">
-        <img src="https://via.placeholder.com/600" />
-      </div>
       <input class="landingpage__button--green landingpage__button--mobile green-btn--mobile" type="button" value="Check it out!" v-on:click="toDashboardPage">
     </section>
   </div>
