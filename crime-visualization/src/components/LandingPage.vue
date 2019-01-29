61f531d41ff0a30ee3649c340c5483484b35f039
Jesse Ma <j.masonoma17@gmail.com>
2019-01-28 23:33:54 -0800

    Update landing page content and styling for mobile breakpoint

--- crime-visualization/src/components/LandingPage.vue
+++ crime-visualization/src/components/LandingPage.vue
@@ -1,15 +1,12 @@
 <template>
   <div id="landingpage">
-    <div class="landingpage__logo-container">
-      <img class="landingpage__image--logo" src="../assets/seacrime-logo.png" alt="SeaCrime - Crime Visualization of Seattle">
-    </div>
     <section class="landingpage__section">
+      <img class="landingpage__image--logo" src="../assets/seacrime-logo.png" alt="SeaCrime - Crime Visualization of Seattle">
       <article class="landingpage__article">
-        <h1 class="landingpage__headline">Introducing SeaCrime.</h1>
-        <p class="landingpage__content">Visualizing Seattle's crime data in graphs</p>
-        <input class="landingpage__button--green landingpage__button--desktop" type="button" value="Check it out!" v-on:click="toDashboardPage">
+        <h1 class="landingpage__headline">Introducing <span class="headline--bold">SeaCrime</span></h1>
+        <p class="landingpage__content">Seattle's crime data in visual graphs</p>
+        <input class="landingpage__button" type="button" value="Check it out" v-on:click="toDashboardPage">
       </article>
-      <input class="landingpage__button--green landingpage__button--mobile green-btn--mobile" type="button" value="Check it out!" v-on:click="toDashboardPage">
     </section>
   </div>
 </template>
