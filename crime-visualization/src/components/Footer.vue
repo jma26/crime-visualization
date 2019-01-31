888316a0a8eeb78a5cfa8b6c6f7a261322f5cb82
Jesse Ma <j.masonoma17@gmail.com>
2019-01-30 17:35:27 -0800

    Add anchor links to social media icons at footer

--- crime-visualization/src/components/Footer.vue
+++ crime-visualization/src/components/Footer.vue
@@ -4,16 +4,19 @@
       <footer class="footer__footer fixed-bottom">
         <ul class="footer__list">
           <li class="footer__list-item">
-            <font-awesome-icon class="footer__icons" :icon="['fab', 'github']" />
+            <a href="https://github.com/jma26" rel="noopener noreferrer" alt="Jesse Ma's GitHub profile" target="_blank">
+              <font-awesome-icon class="footer__icons" size="lg" :icon="['fab', 'github']" />
+            </a>
           </li>
           <li class="footer__list-item">
-            <font-awesome-icon class="footer__icons" :icon="['fab', 'linkedin']" />
+            <a href="https://www.linkedin.com/in/jesse-ma-b2522b84/" rel="noopener noreferrer" alt="Jesse Ma's Linkedin profile" target="_blank">
+              <font-awesome-icon class="footer__icons" size="lg" :icon="['fab', 'linkedin']" />
+            </a>
           </li>
           <li class="footer__list-item">
-            <font-awesome-icon class="footer__icons" :icon="['fab', 'twitter']" />
-          </li>
-          <li class="footer__list-item">
-            <p class="footer__author">Jesse Ma</p> 
+            <a href="https://twitter.com/macode26" rel="noopener noreferrer" alt="Jesse Ma's Twitter profile" target="_blank">
+              <font-awesome-icon class="footer__icons" size="lg" :icon="['fab', 'twitter']" />
+            </a>
           </li>
         </ul>
       </footer>
